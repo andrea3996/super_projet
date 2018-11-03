243c4a95f1ff37a922999dfa0577ab6cb83b4eb6
andrea <mac@172.19.10.203>
2018-11-03 16:58:12 +0100

    Améliorer map et ajouter des images

--- map.cpp
+++ map.cpp
@@ -15,9 +15,6 @@ Map::Map()
 
 }
 
-
-
-
 vector<int> listeNombre(ofstream fichier)
 {
     // Extraire données du fichier
@@ -30,7 +27,7 @@ vector<int> listeNombre(ofstream fichier)
             if (mot != ',')
             {
                 liste.push_back(mot);
-                // insert ne fonctionne pas pour vector faire pushback !!
+                // insert ne fonctionne pas pour vector, faire pushback !!
 
             }
 
@@ -72,11 +69,33 @@ vector<Cellule> listeCellule(vector<liste> liste)
 {
 // renvoit liste de cellules
     for (i
-            if ls (i) == '102'
+            if ls (i) == '2'
              foret
             insert li un carré bleu
 
-            if
+            if ls(i) == '101' or '104' or '102' or '109' or '108'
+                hpipe
+
+            if ls(i)== '4' or '7' or '9'
+               river
+            if ls(i)=='3'
+                wood
+            if ls(i)== '1'
+                plain
+            if ls(i)=='34'
+                greenearthcity
+            if ls(i)== '15' or '16' or '18' or '19' or '20' or '21' or '23'
+                hroad
+            if ls(i)== '30'
+                sea
+            if ls(i)== '33'
+                reef
+            if ls(i)== '36'
+                greenearthairport
+            if ls(i)== '92'
+                greenearthbase
+            if ls(i)== '26'
+                hbridge
 }
 
 /* int** a = new int*[rowCount];
