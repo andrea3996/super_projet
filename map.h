d37b1336af4764a1b11a024fcb3327299e1d1051
Andrea Ledent <andrealedent@gmail.com>
2018-11-03 17:34:43 +0100

    ifstream vs ofstream
    faire bien attention entre les fichiers en LECTURE et en ÉCRITURE
    ici on lit le fichier

--- map.h
+++ map.h
@@ -8,7 +8,7 @@ class Map
 public:
     Map();
 private:
-    std::vector<int> listeMap(std::ofstream & fichier);  // attention, fichier n'est pas copiable
+    std::vector<int> listeMap(std::ifstream & fichier);  // attention, fichier n'est pas copiable
     int rows;
     int column;
     std::vector<std::vector<int>> v;
