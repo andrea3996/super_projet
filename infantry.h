cf4a3afe83f0e14dee98e004824df6b8f2465a33
Nassim Anayat <Nassim.Anayat@ulb.be>
2018-11-15 23:03:27 +0100

    class Infantry en commentaire

--- infantry.h
+++ infantry.h
@@ -4,6 +4,46 @@
 
 class Infantry
 {
+    /*
+    enum ETeam
+    {
+      T_RED,
+      T_BLUE,
+    };
+
+    enum EDirection
+    {
+      DIR_INVALID,
+
+      DIR_LEFT,
+      DIR_RIGHT,
+      DIR_UP,
+      DIR_DOWN,
+    };
+
+    inline EDirection GetDirectionFromOffset(const Vector2i &vOffset)
+    {
+      int x = vOffset(1);
+      int y = vOffset(2);
+
+      EDirection dir = DIR_INVALID;
+      if(x != 0) {
+        dir = (x > 0 ? DIR_RIGHT : DIR_LEFT);
+      } else if(y != 0) {
+        dir = (y > 0 ? DIR_DOWN : DIR_UP);
+      }
+
+      return dir;
+    }
+
+    inline ETeam GetTeamName(ETeam et)
+    {
+      switch(et) {
+      case T_RED:   return "Red";
+      case T_BLUE:   return "Blue";
+      }
+      */
+
 public:
     Infantry();
 };
