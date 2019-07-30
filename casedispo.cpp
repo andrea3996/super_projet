1d071efaaeca9ccaca180941be5b7a0febe28794
Andrea Ledent <andrealedent@gmail.com>
2019-07-30 21:47:38 +0200

    ajout CaseDispo Class

---
+++ casedispo.cpp
@@ -0,0 +1,26 @@
+#include "casedispo.h"
+
+CaseDispo::CaseDispo()
+{
+
+}
+
+std::pair<int, int> CaseDispo::getCelluleDispo()
+{
+    return celluleDispo;
+}
+
+std::pair<int, int> CaseDispo::getCellulePrecedente()
+{
+    return cellulePrecedente;
+}
+
+int CaseDispo::getDistance()
+{
+    return distance;
+}
+
+int CaseDispo::setDistance(int distance)
+{
+    this->distance = distance;
+}
---
+++ casedispo.h
@@ -0,0 +1,21 @@
+#ifndef CASEDISPO_H
+#define CASEDISPO_H
+#include <iostream>
+#include <ostream>
+
+
+class CaseDispo {
+    public :
+        CaseDispo();
+        std::pair<int,int> getCelluleDispo();
+        std::pair<int,int> getCellulePrecedente();
+        int getDistance();
+        int setDistance(int distance);
+    private :
+        std::pair<int,int> celluleDispo;
+        std::pair<int,int> cellulePrecedente;
+        int distance;
+};
+
+
+#endif // CASEDISPO_H
